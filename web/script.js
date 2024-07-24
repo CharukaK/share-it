13a162e4f9a79397187079586023739959848614
CharukaK <charukakarunanayake@gmail.com>
2024-07-24 22:05:43 +0530

    Add drawing functionality

--- web/script.js
+++ web/script.js
@@ -7,16 +7,24 @@
 const PRIMARY_MOUSE_BTN = 1
 
 const canvas = document.querySelector("canvas");
-const context = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
+var lastPosition = undefined;
 
 function init() {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
+
+    // // Draw yellow background
+    // ctx.beginPath();
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 }
 
 function resize() {
+    // const data = ctx.getImageData()
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
+    // ctx.putImageData(data, 0, 0, 0, 0, window.innerWidth, window.innerHeight)
 };
 
 /**
@@ -25,15 +33,30 @@ function resize() {
  * */
 function mouseMove(e) {
     if (e.buttons == PRIMARY_MOUSE_BTN) {
-        console.log("haah")
-        context.fillStyle = 'green';
-        context.beginPath();
-        context.arc(e.x, e.y, 0, 0, 0)
+        if (!lastPosition) {
+            lastPosition = { x: e.offsetX, y: e.offsetY };
+            return;
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(lastPosition.x, lastPosition.y);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.strokeStyle = 'green';
+        ctx.lineWidth = 4;
+        ctx.lineCap = 'round';
+        ctx.stroke();
+        lastPosition = { x: e.offsetX, y: e.offsetY };
     }
+}
+
+function mouseButtonRelease() {
+    lastPosition = undefined;
+    ctx.save()
 }
 
 window.addEventListener('resize', resize);
 window.addEventListener('mousemove', mouseMove)
+window.addEventListener('mouseup', mouseButtonRelease)
 
 init();
 
